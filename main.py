d09f8d54630b45ab9697f40f858907bf51a2a248
SaalihahWebDev <bashirabanu91@gmail.com>
2025-09-16 05:34:31 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+orange=6
+blue=4
+total=10
+left=total-1
+answer=(orange/total)*(blue/left)
+print("Orange balls:",orange,"out of",total)
+print("Blue balls:",orange,"out of",left)
+print("Chance=",answer)
